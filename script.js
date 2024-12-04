976de8fd4b72c62ae6f8ce342fd466f5e3e84d08
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-12-04 22:23:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,22 +16,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
     image.addEventListener("dragover", (e) => {
       e.preventDefault();
-      e.target.classList.add("hovered");
-    });
-
-    image.addEventListener("dragleave", (e) => {
-      e.target.classList.remove("hovered");
     });
 
     image.addEventListener("drop", (e) => {
       e.preventDefault();
-      e.target.classList.remove("hovered");
 
       if (draggedElement && draggedElement !== e.target) {
         const draggedIndex = Array.from(parent.children).indexOf(draggedElement);
         const targetIndex = Array.from(parent.children).indexOf(e.target);
 
-        // Swap the elements in the DOM
+   
         if (draggedIndex < targetIndex) {
           parent.insertBefore(e.target, draggedElement);
           parent.insertBefore(draggedElement, e.target.nextSibling);
